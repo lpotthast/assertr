c3ff32eb076cc8f2f5ef4bdf74b994988f852929
Lukas Potthast <privat@lukas-potthast.de>
2024-06-16 16:46:54 +0200

    Add additional PartialOrd assertions

--- src/assertions/ord.rs
+++ src/assertions/ord.rs
@@ -11,7 +11,7 @@ impl<'t, T: PartialOrd> AssertThat<'t, T> {
         let actual = self.actual.borrowed();
         let expected = &expected;
 
-        if actual < expected {
+        if !(actual < expected) {
             self.fail_with(GenericFailure {
                 arguments: format_args!(
                     "Actual: {actual:#?}\n\nis not less than\n\nExpected: {expected:#?}"
@@ -19,5 +19,94 @@ impl<'t, T: PartialOrd> AssertThat<'t, T> {
             });
         }
         self
+    }
+
+    #[track_caller]
+    pub fn is_greater_than(self, expected: T) -> Self
+    where
+        T: Debug,
+    {
+        let actual = self.actual.borrowed();
+        let expected = &expected;
+
+        if !(actual > expected) {
+            self.fail_with(GenericFailure {
+                arguments: format_args!(
+                    "Actual: {actual:#?}\n\nis not greater than\n\nExpected: {expected:#?}"
+                ),
+            });
+        }
+        self
+    }
+
+    #[track_caller]
+    pub fn is_less_or_equal_to(self, expected: T) -> Self
+    where
+        T: Debug,
+    {
+        let actual = self.actual.borrowed();
+        let expected = &expected;
+
+        if !(actual <= expected) {
+            self.fail_with(GenericFailure {
+                arguments: format_args!(
+                    "Actual: {actual:#?}\n\nis not less or equal to\n\nExpected: {expected:#?}"
+                ),
+            });
+        }
+        self
+    }
+
+    #[track_caller]
+    pub fn is_greater_or_equal_to(self, expected: T) -> Self
+    where
+        T: Debug,
+    {
+        let actual = self.actual.borrowed();
+        let expected = &expected;
+
+        if !(actual >= expected) {
+            self.fail_with(GenericFailure {
+                arguments: format_args!(
+                    "Actual: {actual:#?}\n\nis not greater or equal to\n\nExpected: {expected:#?}"
+                ),
+            });
+        }
+        self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::assert_that;
+
+    #[test]
+    fn is_less_than_succeeds_when_less() {
+        assert_that(3).is_less_than(4);
+    }
+
+    #[test]
+    fn is_greater_than_succeeds_when_greater() {
+        assert_that(7).is_greater_than(6);
+    }
+
+    #[test]
+    fn is_less_or_equal_to_than_succeeds_when_less() {
+        assert_that(3).is_less_or_equal_to(4);
+    }
+
+    #[test]
+    fn is_less_or_equal_to_than_succeeds_when_equal() {
+        assert_that(3).is_less_or_equal_to(3);
+    }
+
+    #[test]
+    fn is_greater_or_equal_to_succeeds_when_greater() {
+        assert_that(7).is_greater_or_equal_to(6);
+    }
+
+    #[test]
+    fn is_greater_or_equal_to_succeeds_when_equal() {
+        assert_that(7).is_greater_or_equal_to(7);
     }
 }
