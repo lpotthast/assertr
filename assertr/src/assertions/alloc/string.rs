1f9a0e65f8c39a56bb4eae01df5ac6c6cde605ee
Lukas Potthast <privat@lukas-potthast.de>
2025-09-17 21:28:17 +0200

    Add negative str assertions

--- assertr/src/assertions/alloc/string.rs
+++ assertr/src/assertions/alloc/string.rs
@@ -7,9 +7,15 @@ use crate::{AssertThat, Mode};
 pub trait StringAssertions {
     fn contains(self, expected: impl AsRef<str>) -> Self;
 
+    fn does_not_contain(self, unexpected: impl AsRef<str>) -> Self;
+
     fn starts_with(self, expected: impl AsRef<str>) -> Self;
 
+    fn does_not_start_with(self, unexpected: impl AsRef<str>) -> Self;
+
     fn ends_with(self, expected: impl AsRef<str>) -> Self;
+
+    fn does_not_end_with(self, unexpected: impl AsRef<str>) -> Self;
 }
 
 impl<M: Mode> StringAssertions for AssertThat<'_, String, M> {
@@ -20,14 +26,35 @@ impl<M: Mode> StringAssertions for AssertThat<'_, String, M> {
     }
 
     #[track_caller]
+    fn does_not_contain(self, unexpected: impl AsRef<str>) -> Self {
+        self.derive(|actual| actual.as_str())
+            .does_not_contain(unexpected);
+        self
+    }
+
+    #[track_caller]
     fn starts_with(self, expected: impl AsRef<str>) -> Self {
         self.derive(|actual| actual.as_str()).starts_with(expected);
         self
     }
 
     #[track_caller]
+    fn does_not_start_with(self, unexpected: impl AsRef<str>) -> Self {
+        self.derive(|actual| actual.as_str())
+            .does_not_start_with(unexpected);
+        self
+    }
+
+    #[track_caller]
     fn ends_with(self, expected: impl AsRef<str>) -> Self {
         self.derive(|actual| actual.as_str()).ends_with(expected);
+        self
+    }
+
+    #[track_caller]
+    fn does_not_end_with(self, unexpected: impl AsRef<str>) -> Self {
+        self.derive(|actual| actual.as_str())
+            .does_not_end_with(unexpected);
         self
     }
 }
@@ -65,6 +92,35 @@ mod tests {
         }
     }
 
+    mod does_not_contain {
+        use crate::prelude::*;
+        use indoc::formatdoc;
+
+        #[test]
+        fn succeeds_when_expected_is_not_contained() {
+            assert_that(String::from("foobar")).does_not_contain("hello");
+        }
+
+        #[test]
+        fn panics_when_expected_is_contained() {
+            assert_that_panic_by(|| {
+                assert_that(String::from("foo bar baz"))
+                    .with_location(false)
+                    .does_not_contain("ar b");
+            })
+            .has_type::<String>()
+            .is_equal_to(formatdoc! {r#"
+                -------- assertr --------
+                Actual: "foo bar baz"
+
+                contains
+
+                Unexpected: "ar b"
+                -------- assertr --------
+            "#});
+        }
+    }
+
     mod starts_with {
         use crate::prelude::*;
         use indoc::formatdoc;
@@ -75,7 +131,7 @@ mod tests {
         }
 
         #[test]
-        fn panics_when_start_is_different() {
+        fn panics_when_start_does_not_match() {
             assert_that_panic_by(|| {
                 assert_that(String::from("foo bar baz"))
                     .with_location(false)
@@ -94,17 +150,46 @@ mod tests {
         }
     }
 
+    mod does_not_start_with {
+        use crate::prelude::*;
+        use indoc::formatdoc;
+
+        #[test]
+        fn succeeds_when_start_does_not_match() {
+            assert_that(String::from("foo bar baz")).starts_with("fo");
+        }
+
+        #[test]
+        fn panics_when_start_matches() {
+            assert_that_panic_by(|| {
+                assert_that(String::from("foo bar baz"))
+                    .with_location(false)
+                    .does_not_start_with("foo bar ba");
+            })
+            .has_type::<String>()
+            .is_equal_to(formatdoc! {r#"
+                    -------- assertr --------
+                    Actual: "foo bar baz"
+
+                    starts with
+
+                    Unexpected: "foo bar ba"
+                    -------- assertr --------
+                "#});
+        }
+    }
+
     mod ends_with {
         use crate::prelude::*;
         use indoc::formatdoc;
 
         #[test]
-        fn succeeds_when_start_matches() {
+        fn succeeds_when_end_matches() {
             assert_that(String::from("foo bar baz")).ends_with("r baz");
         }
 
         #[test]
-        fn panics_when_start_is_different() {
+        fn panics_when_end_is_different() {
             assert_that_panic_by(|| {
                 assert_that(String::from("foo bar baz"))
                     .with_location(false)
@@ -118,6 +203,35 @@ mod tests {
                     does not end with
 
                     Expected: "raz"
+                    -------- assertr --------
+                "#});
+        }
+    }
+
+    mod does_not_end_with {
+        use crate::prelude::*;
+        use indoc::formatdoc;
+
+        #[test]
+        fn succeeds_when_end_does_not_matches() {
+            assert_that(String::from("foo bar baz")).does_not_end_with("bar");
+        }
+
+        #[test]
+        fn panics_when_end_matches() {
+            assert_that_panic_by(|| {
+                assert_that(String::from("foo bar baz"))
+                    .with_location(false)
+                    .does_not_end_with(" baz");
+            })
+            .has_type::<String>()
+            .is_equal_to(formatdoc! {r#"
+                    -------- assertr --------
+                    Actual: "foo bar baz"
+
+                    ends with
+
+                    Unexpected: " baz"
                     -------- assertr --------
                 "#});
         }
